152d2f96ef6f32cb8ceb3873ee0d63bea64b2314
mmcachran <mattmcachran@gmail.com>
2020-07-21 11:27:35 -0400

    Initial Commit.

---
+++ apppresser-custom.php
@@ -0,0 +1,20 @@
+<?php
+/**
+ * AppPresser
+ *
+ * @package   AppPresser
+ * @copyright Copyright(c) 2019, AppPresser LLC
+ * @license http://opensource.org/licenses/GPL-2.0 GNU General Public License, version 2 (GPL-2.0)
+ *
+ * Plugin Name: AppPresser OneSignal
+ * Plugin URI: https://apppresser.com
+ * Description: AppPresser OneSignal Integration
+ * Version: 1.0.0
+ * Author: AppPresser
+ * Author URI: https://apppresser.com
+ * License: GPL2
+ * License URI: http://www.gnu.org/licenses/gpl-2.0.html
+ * Text Domain: apppresser
+ * Domain Path: languages
+ */
+
