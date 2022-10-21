583101d5851ee7453f791bde5d36deb00bfe88bc
modemlooper <modemlooper@gmail.com>
2022-10-21 15:15:50 -0700

    Update apppresser-onesignal.php

--- apppresser-onesignal.php
+++ apppresser-onesignal.php
@@ -9,7 +9,7 @@
  * Plugin Name: AppSignal
  * Plugin URI: https://apppresser.com
  * Description: AppPresser OneSignal Push Notifications
- * Version: 1.0.2
+ * Version: 1.0.3
  * Author: AppPresser
  * Author URI: https://apppresser.com
  * License: GPL2
